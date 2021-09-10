01d3c4575fc4c2fea0171900820a4b3381d98983
avera6756 <avera6756@utm.edu.ec>
2021-09-09 19:02:00 -0500

    Estructura y estilo del footer

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import Search from '../Components/Search';
 import Categories from '../Components/Categories';
 import Carousel from '../Components/Carousel';
 import CarouselItem from '../Components/CarouselItem';
+import Footer from '../Components/Footer';
 
 /* Styles */
 import '../assets/styles/App.scss';
@@ -17,6 +18,7 @@ const App = ()=>(
                 <CarouselItem />
             </Carousel>
         </Categories>
+        <Footer/>
     </div>
 );
 
